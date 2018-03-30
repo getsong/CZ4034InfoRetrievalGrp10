0731ca785e227bbbcc9223c74e6031acf8fc86e4
getsong <getsong13@gmail.com>
2018-03-30 15:59:49 +0800

    description now works

--- amazon/amazon/spiders/AmazonProductSpider.py
+++ amazon/amazon/spiders/AmazonProductSpider.py
@@ -10,26 +10,29 @@ import os
 class AmazonProductSpider(scrapy.Spider):
     name = 'AmazonDeals'
     allowed_domains = ['amazon.com']
-    start_urls = []
+    start_urls = [
+        'https://www.amazon.com/StrengthsFinder-2-0-Tom-Rath/dp/159562015X/ref=lp_2717_1_1/144-7088871-2641201?s=books&ie=UTF8&qid=1522381861&sr=1-1'
+    ]
     
     def parse(self, response):
-        with open(os.path.join('..', 'output_business.txt'), 'r') as infile:
-            url = infile.readline().strip()
-            count = 0
-            while len(url) > 0 and count < 5:
-                self.start_urls.append(url)
-                url = infile.readline().strip()
-                count += 1
+        # with open(os.path.join('..', 'output_business.txt'), 'r') as infile:
+        #     url = infile.readline().strip()
+        #     count = 0
+        #     while len(url) > 0 and count < 5:
+        #         start_urls.append(url)
+        #         url = infile.readline().strip()
+        #         count += 1
         print('urls: ')
         print(self.start_urls)
         items = AmazonItem()
-        title = response.xpath('//h1[@id="title"]/span/text()').extract()
+        #title = response.xpath('//h1[@id="title"]/span/text()').extract()
 ##        sale_price = response.xpath('//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()').extract()
 ##        category = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
 ##        availability = response.xpath('//div[@id="availability"]//text()').extract()
+        rating = response.xpath('//*[@id="reviewSummary"]/div[1]/a/div/div/div[1]/i/span/text()').extract()
         description = response.css('div#bookDescription_feature_div noscript').extract()
         print(description)
-        items['product_description'] = ''.join(title).strip()
+        items['product_description'] = ''.join(description).strip()
 ##        items['product_sale_price'] = ''.join(sale_price).strip()
 ##        items['product_category'] = ','.join(map(lambda x: x.strip(), category)).strip()
 ##        items['product_availability'] = ''.join(availability).strip()
