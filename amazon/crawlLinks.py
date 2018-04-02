fe801bc31e3f6c9f34bd1f2b6751d1617dda696c
getsong <getsong13@gmail.com>
2018-04-02 14:45:12 +0800

    finish sports and busness json

--- amazon/crawlLinks.py
+++ amazon/crawlLinks.py
@@ -8,105 +8,90 @@ from selenium import webdriver
 import re
 import json
 
-driver = webdriver.Chrome("C:/Users/daq11/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome("C:/users/Pan/Downloads/chromedriver.exe")
 
-
-with open("C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/amazon/output_sports.txt") as f:
-    links = [x.strip('\n') for x in f.readlines()][202:300] # should continue from 202
+#
+# crawl text
+#
+with open("C:/git/CZ4034InfoRetrievalGrp10/amazon/output_cook.txt") as f:
+    links = [x.strip('\n') for x in f.readlines()][:2] # should continue from here
 # try crawl index
 f.close()
 
 # for each category, initialize one json file
 # =============================================================================
-# with open("sports_new.json", mode='w', encoding='utf-8') as f:
-#     json.dump([], f)
-# f.close()
+with open("cook.json", mode='w', encoding='utf-8') as f:
+    json.dump([], f)
+f.close()
 # =============================================================================
 
 json_data = []
 for link in links:
-    
+
     data_dump = []
     driver.get(link)
-    
+
     try:
         title_span = driver.find_element_by_xpath('//*[@id="productTitle"]')
         title = title_span.get_attribute('innerHTML')
     except:
-        f_e = open('error record.txt','a')
+        f_e = open('error record_cook.txt','a')
         f_e.write(''.join([link,'\n']))
         f_e.close()
         continue
-    
+
     try:
-        rating_box = driver.find_element_by_xpath('//*[@id="reviewSummary"]/div[2]/span/a/span')    
+        rating_box = driver.find_element_by_xpath('//*[@id="reviewSummary"]/div[2]/span/a/span')
         rating_score = rating_box.get_attribute('innerHTML')
     except:
         try:
             rating_box = driver.find_element_by_xpath('//*[@id="dp-no-customer-review-yet"]')
             rating_score = rating_box.get_attribute('innerHTML')
         except:
-            f_e = open('error record.txt','a')
+            f_e = open('error record_cook.txt','a')
             f_e.write(''.join([link,'\n']))
             f_e.close()
-            continue            
-    
+            continue
+
     try:
         driver.switch_to_frame(driver.find_element_by_id("bookDesc_iframe"))
         review = driver.find_element_by_xpath('//*[@id="iframeContent"]')
         review_text = review.get_attribute('innerHTML')
         review = re.sub('<.*?>', '', review_text)
     except:
-        f_e = open('error record.txt','a')
+        f_e = open('error record_cook.txt','a')
         f_e.write(''.join([link,'\n']))
         f_e.close()
-        continue 
-    
+        continue
+
     data = {
-				'product_name':title,
-				'description':review,
-				'ratings':rating_score,
+			'product_name':title,
+			'description':review,
+			'ratings':rating_score,
             'url':link
 			}
     #data_dump.append(data)
-        
-        
-    with open("sports.json", mode='r', encoding='utf-8') as feedsjson:
+
+
+    with open("cook.json", mode='r', encoding='utf-8') as feedsjson:
         feeds = json.load(feedsjson)
-    with open("sports.json", mode='w', encoding='utf-8') as feedsjson:
+    with open("cook.json", mode='w', encoding='utf-8') as feedsjson:
         feeds.append(data)
         json.dump(feeds, feedsjson,indent=4)
     feedsjson.close()
-    
-    
-# =============================================================================
-#     product_name
-#     description
-#     ratings
-# =============================================================================
 
 
 
+# crawl links
 
-
-# =============================================================================
-# # =============================================================================
-# # # business - personal finance 60 entries
-# # str1 = "https://www.amazon.com/s/ref=lp_2717_pg_"
-# # str2 = "?rh=n%3A283155%2Cn%3A%211000%2Cn%3A3%2Cn%3A2717&page="
-# # str3 = "&ie=UTF8&qid=1522379724"
-# # =============================================================================
-# 
-# 
-# 
 # type_list = {
 #         "business":"3A3",
 #         "sports":"3A26",
 #         "food":"3A6",
 #         "literature":"3A17"
 #             }
-# 
-# lp_list = [2159,10399,10129] # refer to below index, put suitable index list in
+#
+# lp_list = [2717,2675,2665,2698,2581] # refer to below index, put suitable index list in
 # # 2717 business - perfonal finance
 # # 2675 business - management
 # # 2665 business - investing
@@ -126,33 +111,51 @@ for link in links:
 # # 2159 literature - drama
 # # 10399 literature - classic
 # # 10129 literature - contemp
-# 
-# for j in lp_list:   
-# 
+#
+# for j in lp_list:
+#
 #     str1 = "https://www.amazon.com/s/ref=lp_"
 #     str2 = str(j)
 #     str3 = "_pg_"
 #     # need str4 for page
 #     str5 = "?rh=n%3A283155%2Cn%3A%211000%2Cn%"
-#     str6 = type_list["literature"]  # need to change with different categories
+#     str6 = type_list["business"]  # need to change with different categories
 #     str7 = "%2Cn%3A"
 #     str8 = str(j)
 #     str9 = "&page="
 #     # need str9 for page
 #     str11 = "&ie=UTF8&qid=1522382251"
-#         
+#
 #     href_list = []
-#     for i in range(1,101):  # extract page 1 to 100
-#         driver.get(''.join([str1,str2,str3,str(i),str5,str6,str7,str8,str9,str(i),str11]))  
+#     for i in range(1,61):  # extract page 1 to 100
+#         href_list = []
+#         driver.get(''.join([str1,str2,str3,str(i),str5,str6,str7,str8,str9,str(i),str11]))
 #         links = driver.find_elements_by_css_selector(".a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal")
 #         for link in links:
 #             href = link.get_attribute('href')
 #             href_list.append(href)
-#                 
-#             
-#     text_file = open("output_sports.txt", "a")
-#     for href in href_list:
-#         text_file.write(''.join([href,'\n']))
-#     text_file.close()
-# =============================================================================
+#
+#         text_file = open("output_business.txt", "a")
+#         for href in href_list:
+#             text_file.write(''.join([href,'\n']))
+#         text_file.close()
+#
+#         text_file = open("progress_business.txt", "a")
+#         text_file.write(''.join(["list", str(j), "page", str(i), "append finish","\n"]))
+#         text_file.close()
+#
+#         print("list", j, "page", i, "append finish")
+
+#remove duplicates
+# with open("C:/git/CZ4034InfoRetrievalGrp10/amazon/output_cook.txt") as f:
+#     links = [x.strip('\n') for x in f.readlines()]
+# f.close()
+# links = set(links)
+# print(len(links))
+# text_file = open("output_cook.txt", "w")
+# for link in links:
+#     text_file.write(''.join([link, "\n"]))
+# text_file.close()
+
+
     
