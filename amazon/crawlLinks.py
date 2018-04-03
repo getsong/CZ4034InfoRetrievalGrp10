9e482d7e75eed3829c1466a98bc3dc27c71415ee
Aiqing1130 <daq1130@hotmail.com>
2018-04-03 19:09:06 +0800

    extraction done except cook add_on

--- amazon/crawlLinks.py
+++ amazon/crawlLinks.py
@@ -8,13 +8,13 @@ from selenium import webdriver
 import re
 import json
 
-driver = webdriver.Chrome("C:/users/Pan/Downloads/chromedriver.exe")
+driver = webdriver.Chrome("C:/Users/daq11/Downloads/chromedriver_win32/chromedriver.exe")
 
 #
 # crawl text
 #
-with open("C:/git/CZ4034InfoRetrievalGrp10/amazon/output_literature.txt") as f:
-    links = [x.strip('\n') for x in f.readlines()][1:] # should continue from here
+with open("C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/amazon/output_cook_addon.txt") as f:
+    links = [x.strip('\n') for x in f.readlines()] # should continue from here
 # try crawl index
 f.close()
 
@@ -27,7 +27,6 @@ f.close()
 
 json_data = []
 for link in links:
-
     data_dump = []
     driver.get(link)
 
@@ -35,7 +34,7 @@ for link in links:
         title_span = driver.find_element_by_xpath('//*[@id="productTitle"]')
         title = title_span.get_attribute('innerHTML')
     except:
-        f_e = open('error record_literature.txt','a')
+        f_e = open('error record_cook.txt','a')
         f_e.write(''.join([link,'\n']))
         f_e.close()
         continue
@@ -48,7 +47,7 @@ for link in links:
             rating_box = driver.find_element_by_xpath('//*[@id="dp-no-customer-review-yet"]')
             rating_score = rating_box.get_attribute('innerHTML')
         except:
-            f_e = open('error record_literature.txt','a')
+            f_e = open('error record_cook.txt','a')
             f_e.write(''.join([link,'\n']))
             f_e.close()
             continue
@@ -59,7 +58,7 @@ for link in links:
         review_text = review.get_attribute('innerHTML')
         review = re.sub('<.*?>', '', review_text)
     except:
-        f_e = open('error record_literature.txt','a')
+        f_e = open('error record_cook.txt','a')
         f_e.write(''.join([link,'\n']))
         f_e.close()
         continue
@@ -74,9 +73,9 @@ for link in links:
     #data_dump.append(data)
 
 
-    with open("literature.json", mode='r', encoding='utf-8') as feedsjson:
+    with open("cook.json", mode='r', encoding='utf-8') as feedsjson:
         feeds = json.load(feedsjson)
-    with open("literature.json", mode='w', encoding='utf-8') as feedsjson:
+    with open("cook.json", mode='w', encoding='utf-8') as feedsjson:
         feeds.append(data)
         json.dump(feeds, feedsjson,indent=4)
     feedsjson.close()
@@ -92,7 +91,7 @@ for link in links:
 #         "literature":"3A17"
 #             }
 #
-# lp_list = [2717,2675,2665,2698,2581] # refer to below index, put suitable index list in
+# lp_list = [4245] # refer to below index, put suitable index list in
 # # 2717 business - perfonal finance
 # # 2675 business - management
 # # 2665 business - investing
@@ -109,10 +108,11 @@ for link in links:
 # # 4257 food - quick&easy
 # # 4252 food - cooking methods
 # # 4336 food - vegetarian
+# # 4245 food - outdoor cooking
 # # 2159 literature - drama
 # # 10399 literature - classic
 # # 10129 literature - contemp
-#
+
 # for j in lp_list:
 #
 #     str1 = "https://www.amazon.com/s/ref=lp_"
@@ -120,7 +120,7 @@ for link in links:
 #     str3 = "_pg_"
 #     # need str4 for page
 #     str5 = "?rh=n%3A283155%2Cn%3A%211000%2Cn%"
-#     str6 = type_list["business"]  # need to change with different categories
+#     str6 = type_list["food"]  # need to change with different categories
 #     str7 = "%2Cn%3A"
 #     str8 = str(j)
 #     str9 = "&page="
@@ -136,27 +136,30 @@ for link in links:
 #             href = link.get_attribute('href')
 #             href_list.append(href)
 #
-#         text_file = open("output_literature.txt", "a")
+#         text_file = open("output_cook_addon.txt", "a")
 #         for href in href_list:
 #             text_file.write(''.join([href,'\n']))
 #         text_file.close()
 #
-#         text_file = open("progress_literature.txt", "a")
+#         text_file = open("progress_cook_addon.txt", "a")
 #         text_file.write(''.join(["list", str(j), "page", str(i), "append finish","\n"]))
 #         text_file.close()
 #
 #         print("list", j, "page", i, "append finish")
 
 #remove duplicates
-# with open("C:/git/CZ4034InfoRetrievalGrp10/amazon/output_literature.txt") as f:
+# with open("C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/amazon/output_business_origin.txt") as f:
 #     links = [x.strip('\n') for x in f.readlines()]
 # f.close()
 # links = set(links)
 # print(len(links))
-# text_file = open("output_literature.txt", "w")
+# text_file = open("C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/amazon/output_business_origin.txt", "w")
 # for link in links:
 #     text_file.write(''.join([link, "\n"]))
 # text_file.close()
 
-
-    
+# check json file length
+# with open("business_final.json", mode='r', encoding='utf-8') as feedsjson:
+#     feeds = json.load(feedsjson)
+#     print(len(feeds))
+#
