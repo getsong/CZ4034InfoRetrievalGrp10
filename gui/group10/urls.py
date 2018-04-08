ed5773066227fafe6bad97920acdba7f4b4c29f5
getsong <getsong13@gmail.com>
2018-04-08 17:15:06 +0800

    change template name to form.html and added search view

--- gui/group10/urls.py
+++ gui/group10/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^/search', views.search, name='search'),
 ]
--- gui/group10/views.py
+++ gui/group10/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'group10/test2.html')
+    return render(request, 'group10/form.html')
+
+
+def search(request):
+    if request.method == 'POST':
+        result = "Mock Result"
+        try:
+            # do something with user
+            html = ("<H1>%s</H1>", result)
+            return HttpResponse(html)
+        except:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'group10/form.html')
