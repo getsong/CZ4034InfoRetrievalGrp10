3c6c2e2cccf6721770132a796ff070da89d5b3c0
getsong <getsong13@gmail.com>
2018-04-08 17:19:37 +0800

    fix bug: url should be defined under project

--- gui/group10/urls.py
+++ gui/group10/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/search', views.search, name='search'),
 ]
--- gui/gui/urls.py
+++ gui/gui/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search, name='search'),
     path('admin/', admin.site.urls),
 ]
