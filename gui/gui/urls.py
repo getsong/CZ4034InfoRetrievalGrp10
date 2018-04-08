6565df6a54be52f3433ee55f82478e32e364d669
getsong <getsong13@gmail.com>
2018-04-08 14:23:18 +0800

    create the first view

--- gui/gui/urls.py
+++ gui/gui/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from group10 import views
+from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
