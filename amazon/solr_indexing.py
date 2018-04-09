e7eec21164b4690419f384073736a28b66155694
Aiqing1130 <daq1130@hotmail.com>
2018-04-09 16:00:54 +0800

    process request json

--- amazon/solr_indexing.py
+++ amazon/solr_indexing.py
@@ -6,15 +6,37 @@ Created on Fri Mar 30 15:28:00 2018
 """
 
 import pysolr,json,argparse
-parser = argparse.ArgumentParser(description='load json into python.')
-parser.add_argument('C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/amazon/json_trial.json', metavar='input', type=str, help='json input file')
-parser.add_argument('solr_url', metavar='url', type=str, help='solr URL')
+# =============================================================================
+# parser = argparse.ArgumentParser(description='load json into python.')
+# parser.add_argument('C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/preprocess/amazon_Stemmed.json', metavar='input', type=str, help='json input file')
+# parser.add_argument('solr_url', metavar='url', type=str, help='solr URL')
+# 
+# args = parser.parse_args()
+# solr = pysolr.Solr(args.solr_url, timeout=10)
+# 
+# items = json.load(open(args.input_file))
+# for item in items:
+#   item['id'] = item['url']
+# 
+# solr.add(items)
+# =============================================================================
 
-args = parser.parse_args()
-solr = pysolr.Solr(args.solr_url, timeout=10)
 
-items = json.load(open(args.input_file))
+
+solr_instance = pysolr.Solr('http://localhost:8983/solr/amazon', timeout=10)
+json_filename = 'C:/Users/daq11/Dropbox/developer/CZ4034/CZ4034InfoRetrievalGrp10/preprocess/amazon_Stemmed.json'
+argws = {
+    'commit': 'true',
+    'extractOnly': False,
+    'Content-Type': 'application/json',
+}
+with open(json_filename, 'rb') as f:
+    solr_instance.extract(f, **argws)
+    solr_instance.commit()
+
+with open(json_filename, mode='r', encoding='utf-8') as feedsjson:    
+    items = json.load(feedsjson)
 for item in items:
   item['id'] = item['url']
 
-solr.add(items)
+solr_instance.add(items)
--- gui/group10/views.py
+++ gui/group10/views.py
@@ -18,6 +18,7 @@ def search(request):
         try:
             queryList = p.preprocess(request.POST.get("search"))
             query = ' '.join(queryList)
+            result 
             print("query:", query)
             contextDict['result'] = "A Lot Of Results Are Retrieved for keywords: " + query
             # TODO: do something with data
@@ -27,3 +28,20 @@ def search(request):
     else:
         contextDict['result'] = ""
         return render(request, 'group10/form.html', context=contextDict)
+    
+    
+    
+    
+import requests
+import json
+
+response = requests.get("http://localhost:8983/solr/amazon/select?df=product_name&q=perfect%20AND%20cook")
+json_data = json.loads(response.text)
+docs = json_data['response']['docs']
+no_docs = json_data['response']['numFound']
+for i in range(no_docs):
+    doc = docs[i]
+    print("book title:",doc['product_name'][0])
+    print("description:",doc['description'][0])
+    print("rating:",doc['ratings'][0])
+    print("url link:",doc['url'][0],'\n')
