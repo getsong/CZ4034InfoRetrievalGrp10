1db3d2b922fca501be727f9cec43448f35d1a522
getsong <getsong13@gmail.com>
2018-04-10 21:26:07 +0800

    add spell checker to UI; added comments for the 2 views

--- gui/group10/views.py
+++ gui/group10/views.py
@@ -118,6 +118,7 @@ def spellingCheck(str1):
 
 
 def index(request):
+    """show index page, no search result should be displayed and search bar is in the center of the page"""
     contextDict = {}
     contextDict['result'] = ""
     contextDict['css'] = indexStyle
@@ -125,16 +126,31 @@ def index(request):
 
 
 def search(request):
+    """show search page"""
     contextDict = {}
-    p = Preprocessor()
-    search_query = []
-    isSpellingWrong = False
+
     if request.method == 'POST':
+        # if there is query (which is always through POST method)
         try:
             startTime = time.time()
-            queryList = p.preprocess(request.POST.get("search"))
-            search_query.append(' '.join(queryList))
-            print('cook:', request.POST.get("cook"), type(request.POST.get("cook")))
+
+            # get what user typed for query
+            userQuery = request.POST.get("search")
+
+            # check spelling
+            isSpellingWrong = False
+            if len(userQuery) > 0:
+                returnStr = spellingCheck(userQuery)
+                if returnStr != "?":
+                    isSpellingWrong = True
+                else:
+                    isSpellingWrong = False
+
+            # preprocess data by tokenizing, lemmatizing, etc., returning a list of lemmas
+            p = Preprocessor()
+            queryList = p.preprocess(userQuery)
+
+            # if list of lemmas is not too long, try advanced search of 2-grams and combinations
             if len(queryList) <= 10:
                 twoGramList = [''.join(['\"', queryList[i], ' ', queryList[i + 1], '\"^10']) for i in
                                range(len(queryList) - 1)]
@@ -143,22 +159,26 @@ def search(request):
                 query = ' '.join(twoGramList + combiList + queryList)
             else:
                 query = ' '.join(queryList)
-            if len(queryList) == 1:
-                return_str = spellingCheck(queryList[0])
-                if return_str != "?":
-                    isSpellingWrong = True
-                else:
-                    isSpellingWrong = False
+
+            # replace quotation mark and whitespace with chars legit in the request url
             query = re.sub(r'\"', '%22', query)
             query = re.sub(r' ', '%20', query)
-            #raise Exception("Don't get response")
+            # raise Exception("Don't get response")
+
+            # get the query response from solr, get the records from response
             response = requests.get("http://localhost:8983/solr/amazon/select?df=product_name&q=" + query + "&rows=100")
             json_data = json.loads(response.text)
             docs = json_data['response']['docs']
             no_docs = len(docs)
-            result = "Results Retrieved:<br>"
+
+            # create the starting string of results
+            result = ""
+            if isSpellingWrong:
+                result += ''.join(["<div> Do you mean: ", return_str, "?<br></div><br>"])
+            result += "Results Retrieved:<br>"
             result += "<ol>"
-            # retrieve DocID from results
+
+            # retrieve the list of id, product_name and descriptions from results
             id_list = []
             book_list = []
             desc_list = []
@@ -167,32 +187,38 @@ def search(request):
                 id_list.append(doc['DocID'][0])
                 book_list.append(doc['product_name'][0])
                 desc_list.append(doc['description'][0])
-            # retreive corresponding original documents from json file by id
+
+            # retrieve corresponding original documents from json file by ranked ids
             feeds = []
-            with open(os.path.join(base_dir, "crawl", "amazon_all_withID.json"), mode='r', encoding='utf-8') as feedsjson:
+            with open(os.path.join(base_dir, "crawl", "amazon_all_withID.json"), mode='r', encoding='utf-8') \
+                    as feedsjson:
                 feeds_all = json.load(feedsjson)
                 for doc_id in id_list:
                     feeds.append(feeds_all[int(doc_id)])
             print("querying speed: ", time.time()-startTime, "seconds")
-            # display in HTMl
-            if isSpellingWrong:
-                result += ''.join(["<div> do you mean: ", return_str, "<br></div>"])
+
+            # add records into result one by one, each record is an item in ordered list
             for i in range(no_docs):
                 result += "<li><ul>"
                 doc = feeds[i]
                 url = doc['url']
-                result = ''.join([result, "<li >", "<a style='color:darkblue;font-size=35px;font-weight:bold;' href=\"{}\">".format(url), doc['product_name'], "</a></li>"])
+                result = ''.join([result, "<li >",
+                                  "<a style='color:darkblue;font-size=35px;font-weight:bold;' href=\"{}\">".format(url),
+                                  doc['product_name'], "</a></li>"])
                 result = ''.join([result, "<li>Description: ", doc['description'], "</li>"])
                 result = ''.join([result, "<li>Customer rating: ", str(doc['ratings']), "</li>"])
                 result += "</ul></li><br>"
             result += "</ol>"
 
+            # pass the result string and search css style to template and render
             contextDict['result'] = result
             contextDict['css'] = searchStyle
             return render(request, 'group10/form.html', context=contextDict)
         except:
+            # if bug, print stack trace
             traceback.print_exc()
     else:
+        # if no query, same as index page
         contextDict['result'] = ""
         contextDict['css'] = indexStyle
         return render(request, 'group10/form.html', context=contextDict)
